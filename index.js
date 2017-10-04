9b2aff12dc440af07597cea6da735de7695ff635
gitluky <jluk87@gmail.com>
2017-10-04 07:17:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ function viewCart() {
       }
       else {
         cartListChunks[cart.length-1] = ` and ${cartListChunks[cart.length-1]}.`
-        var cartString = `In your cart, you have${cartListChunks[0]},${cartListChunks[1]},${cartListChunks[cart.length-1]}`
+        var cartString = `In your cart, you have${cartListChunk}`
         console.log(cartString)
       }
   }
